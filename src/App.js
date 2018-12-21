c5245bd3e0b76b1b44e99a15bf4a9dbd3ad16d05
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-22 02:53:04 +0300

    setting up + adding the menu buttons and other stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import anime from 'animejs';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+
+  var basicTimeline = anime.timeline();
+
+  basicTimeline
+    .add({
+      targets: '#menu-box',
+      duration: 700,
+      // duration: 0,
+      height: '100',
+      borderRadius: '3',
+      width: '90%',
+      opacity: '1',
+      easing: 'easeOutExpo',
+      // loop: true
+    })
+    .add({
+      targets: '.menu-button',
+      duration: 300,
+      // duration: 0,
+      height: '90%',
+      borderRadius: '3',
+      width: '90%',
+      opacity: '1',
+      easing: 'easeOutExpo',
+    })
+    .add({
+      targets: '#menu-box',
+      // duration: 500,
+      duration: 200,
+      border: '9px solid white',
+      easing: 'easeOutExpo',
+    })
+    .add({
+      targets: '#menu-box',
+      // duration: 500,
+      duration: 200,
+      border: '0px solid white',
+      easing: 'easeOutExpo',
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div id="menu-box" style={{backgroundColor: '#43484e', height: 20, width: 20, borderRadius: '100', margin: 10, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }} >
+            <div id="menu-create-button" class="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
+            <div id="menu-metamask-button" class="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, marginRight: 0, marginLeft: 0, opacity: 1 }} />
+            <div id="menu-info-button" class="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
+          </div>
       </div>
     );
   }
