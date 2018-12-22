5013bd7b729b479cf4bde446700ad43697e9e46f
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-23 02:07:25 +0300

    adding meta mask image along with the menu buttons and also

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import logo from './logo.svg';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import Button from '@material-ui/core/Button';
+import metamaskIcon from './metamask.png';
+import infoIcon from './info.png';
+import mainmenuIcon from './main.png';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const styles = theme => ({
   button: {
@@ -35,6 +39,9 @@ class App extends Component {
   componentDidMount() {
 
   var basicTimeline = anime.timeline();
+  var motionDiv = anime.timeline();
+
+
 
   // animating the logo
   anime({
@@ -82,7 +89,7 @@ class App extends Component {
       // loop: true
     })
     .add({
-      targets: '.main-textarea ,.main-datepicker, #send-button',
+      targets: '.main-textarea ,.main-datepicker, #send-button, .material-menu-button',
       // duration: 500,
       duration: 400,
       opacity: 1,
@@ -113,11 +120,29 @@ class App extends Component {
           </div> */}
 
           <div id="menu-box" style={{backgroundColor: '#43484e', height: 20, width: 0, borderRadius: '100', margin: 10, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }}>
-            <div id="menu-create-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
-            <div id="menu-metamask-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, marginRight: 0, marginLeft: 0, opacity: 1 }} />
-            <div id="menu-info-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
+            <div id="menu-create-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 0 }} >
+              <Tooltip title="Main" placement="top">
+                <Button id="create-button" className="material-menu-button" variant="contained" href="#contained-buttons" style={{ backgroundColor:'#DCDCDC', color: 'black', width: '100%', height: '100%', fontSize: 12, opacity: 1, zIndex: 2}}>
+                  <img src={mainmenuIcon} height="60"/>
+                </Button>
+              </Tooltip>
+            </div>
+            <div id="menu-metamask-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, marginRight: 0, marginLeft: 0, opacity: 0 }}>
+              <Tooltip title="Use Metamask" placement="top">
+                <Button id="via-metamask-button" className="material-menu-button" variant="contained" href="#contained-buttons" style={{ backgroundColor:'#DCDCDC', color: 'black', width: '100%', height: '100%', fontSize: 12, opacity: 1, zIndex: 2}}>
+                  <img src={metamaskIcon} height="50" style={{top: 3}}/>
+                </Button>
+              </Tooltip>
+            </div>
+            <div id="menu-info-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 0 }}>
+              <Tooltip title="@About" placement="top">
+                <Button id="info-button" className="material-menu-button"  variant="contained" href="#contained-buttons" style={{ backgroundColor:'#DCDCDC', color: 'black', width: '100%', height: '100%', fontSize: 12, opacity: 1, zIndex: 2}}>
+                  <img src={infoIcon} height="60"/>
+                </Button>
+              </Tooltip>
+            </div>
           </div>
-          <div id="body-box" style={{backgroundColor: '#43484e', height: 20, width: '90%', borderRadius: '100', margin: 10, marginTop: 2, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }}>
+          <div id="body-box" style={{backgroundColor: '#43484e', height: 20, width: '90%', borderRadius: '100', margin: 10, marginTop: 2, opacity: 0, padding: 0, maxWidth: 500, display: 'flex', position: 'relative' }}>
             <textarea className="main-textarea">
 
             </textarea>
