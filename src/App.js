77e6f3b6fefe99aab2045236f65f6176a493af69
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-22 04:05:04 +0300

    adding a dummy logo and animating it and animating other stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import anime from 'animejs';
 import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
 
@@ -8,21 +9,31 @@ class App extends Component {
 
   var basicTimeline = anime.timeline();
 
+  // animating the logo
+  anime({
+    delay: 1500,
+    targets: '#logo',
+    opacity: 1,
+    easing: 'easeInOutSine',
+    duration: 1500,
+  });
+
   basicTimeline
     .add({
+      delay: 150,
       targets: '#menu-box',
       duration: 700,
       // duration: 0,
       height: '100',
       borderRadius: '3',
-      width: '90%',
+      width: '95%',
       opacity: '1',
-      easing: 'easeOutExpo',
+      easing: 'easeOutCirc'
       // loop: true
     })
     .add({
       targets: '.menu-button',
-      duration: 300,
+      duration: 300, 
       // duration: 0,
       height: '90%',
       borderRadius: '3',
@@ -31,16 +42,27 @@ class App extends Component {
       easing: 'easeOutExpo',
     })
     .add({
-      targets: '#menu-box',
+      targets: '#body-box',
+      duration: 700,
+      // duration: 0,
+      height: '500',
+      borderRadius: '3',
+      width: '95%',
+      opacity: '1',
+      easing: 'easeOutCirc'
+      // loop: true
+    })
+    .add({
+      targets: '#menu-box ,#body-box',
       // duration: 500,
-      duration: 200,
-      border: '9px solid white',
+      duration: 400,
+      border: '#9c9c9c',
       easing: 'easeOutExpo',
     })
     .add({
-      targets: '#menu-box',
+      targets: '#menu-box ,#body-box',
       // duration: 500,
-      duration: 200,
+      duration: 400,
       border: '0px solid white',
       easing: 'easeOutExpo',
     })
@@ -49,10 +71,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <div id="menu-box" style={{backgroundColor: '#43484e', height: 20, width: 20, borderRadius: '100', margin: 10, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }} >
-            <div id="menu-create-button" class="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
-            <div id="menu-metamask-button" class="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, marginRight: 0, marginLeft: 0, opacity: 1 }} />
-            <div id="menu-info-button" class="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
+
+          <div id="logo" style={{opacity: 0}}> 
+            <img src={logo} className="logo" alt="logo" height="80"/>
+          </div>
+
+          <div id="menu-box" style={{backgroundColor: '#43484e', height: 20, width: 20, borderRadius: '100', margin: 10, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }}>
+            <div id="menu-create-button" className="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
+            <div id="menu-metamask-button" className="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, marginRight: 0, marginLeft: 0, opacity: 1 }} />
+            <div id="menu-info-button" className="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
+          </div>
+          <div id="body-box" style={{backgroundColor: '#43484e', height: 20, width: '90%', borderRadius: '100', margin: 10, marginTop: 2, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }}>
+
           </div>
       </div>
     );
