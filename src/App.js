9c8db729386b7e8f79dba9dee92b453b3da89932
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-22 05:37:37 +0300

    did many stuff with the design

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import anime from 'animejs';
 import './App.css';
 import logo from './logo.svg';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import Button from '@material-ui/core/Button';
 
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    
+  }
+
+  state = {
+    startDate: new Date()
+  };
+
+  handleChange = (date) => {
+    this.setState({
+      startDate: date
+    });
+  }
 
   componentDidMount() {
 
@@ -18,20 +45,22 @@ class App extends Component {
     duration: 1500,
   });
 
+  anime({
+    delay: 150,
+    targets: '#menu-box',
+    duration: 700,
+    // duration: 0,
+    borderRadius: '3',
+    width: '95%',
+    height: '100',
+    opacity: '1',
+    easing: 'easeOutExpo',
+    // loop: true
+  });
+
   basicTimeline
     .add({
-      delay: 150,
-      targets: '#menu-box',
-      duration: 700,
-      // duration: 0,
-      height: '100',
-      borderRadius: '3',
-      width: '95%',
-      opacity: '1',
-      easing: 'easeOutCirc'
-      // loop: true
-    })
-    .add({
+      delay: 500,
       targets: '.menu-button',
       duration: 300, 
       // duration: 0,
@@ -43,7 +72,7 @@ class App extends Component {
     })
     .add({
       targets: '#body-box',
-      duration: 700,
+      duration: 400,
       // duration: 0,
       height: '500',
       borderRadius: '3',
@@ -53,10 +82,17 @@ class App extends Component {
       // loop: true
     })
     .add({
+      targets: '.main-textarea ,.main-datepicker, #send-button',
+      // duration: 500,
+      duration: 400,
+      opacity: 1,
+      easing: 'easeOutExpo',
+    })
+    .add({
       targets: '#menu-box ,#body-box',
       // duration: 500,
       duration: 400,
-      border: '#9c9c9c',
+      border: '0px solid #9c9c9c',
       easing: 'easeOutExpo',
     })
     .add({
@@ -72,17 +108,32 @@ class App extends Component {
     return (
       <div className="App">
 
-          <div id="logo" style={{opacity: 0}}> 
-            <img src={logo} className="logo" alt="logo" height="80"/>
-          </div>
+          {/* <div id="logo" style={{opacity: 0}}> 
+            <img src={logo} className="logo" alt="logo" height="50"/>
+          </div> */}
 
-          <div id="menu-box" style={{backgroundColor: '#43484e', height: 20, width: 20, borderRadius: '100', margin: 10, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }}>
-            <div id="menu-create-button" className="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
-            <div id="menu-metamask-button" className="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, marginRight: 0, marginLeft: 0, opacity: 1 }} />
-            <div id="menu-info-button" className="menu-button" style={{backgroundColor: '#EBEBEB', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
+          <div id="menu-box" style={{backgroundColor: '#43484e', height: 20, width: 0, borderRadius: '100', margin: 10, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }}>
+            <div id="menu-create-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
+            <div id="menu-metamask-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, marginRight: 0, marginLeft: 0, opacity: 1 }} />
+            <div id="menu-info-button" className="menu-button" style={{backgroundColor: 'white', height: '0', width: '90%', borderRadius: '100', margin: 5, opacity: 1 }} />
           </div>
           <div id="body-box" style={{backgroundColor: '#43484e', height: 20, width: '90%', borderRadius: '100', margin: 10, marginTop: 2, opacity: 0, padding: 0, maxWidth: 500, display: 'flex' }}>
+            <textarea className="main-textarea">
 
+            </textarea>
+            <DatePicker
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={15}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                timeCaption="time"
+                className="main-datepicker"
+            />
+            <Button id="send-button" variant="contained" href="#contained-buttons" style={{marginTop: 13, backgroundColor:'#608e60', color: 'white', width: 150, height: 50, fontSize: 20, opacity: 0}}>
+              SEND
+            </Button>
           </div>
       </div>
     );
