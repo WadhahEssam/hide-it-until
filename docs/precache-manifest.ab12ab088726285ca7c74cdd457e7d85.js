1694896728842c45a6c30197878ae0b9a5810967
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-23 03:57:16 +0300

    adding the docs folder

---
+++ docs/precache-manifest.ab12ab088726285ca7c74cdd457e7d85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2b22a6b03c61a9fe621b97eca4ebb84",
+    "url": "./static/media/metamask.d2b22a6b.png"
+  },
+  {
+    "revision": "43818593d80c344b6587",
+    "url": "./static/css/main.57a152fa.chunk.css"
+  },
+  {
+    "revision": "af0091ca8b2c1918fffb8a80951bb998",
+    "url": "./static/media/github.af0091ca.svg"
+  },
+  {
+    "revision": "42b60a6b0bbe613b2395",
+    "url": "./static/js/1.42b60a6b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "43818593d80c344b6587",
+    "url": "./static/js/main.43818593.chunk.js"
+  },
+  {
+    "revision": "b18635c6a20c0ba1a6bb583fab301c7c",
+    "url": "./static/media/info.b18635c6.png"
+  },
+  {
+    "revision": "89f2c325cf1daaa9fa55d583a5ec93b0",
+    "url": "./static/media/main.89f2c325.png"
+  },
+  {
+    "revision": "419fa2299b2d45a647f0cc385bb56f79",
+    "url": "./static/media/upload.419fa229.svg"
+  },
+  {
+    "revision": "42b60a6b0bbe613b2395",
+    "url": "./static/css/1.ab34cb0c.chunk.css"
+  },
+  {
+    "revision": "b16c8a8c942027b1dd29992e993ca693",
+    "url": "./index.html"
+  }
+];
